93c60b81fa1d2bdc94a0b3987c4c1ab6472856c2
YounsiAhmed <hammoua07@gmail.com>
2022-07-18 02:14:24 +0100

    dgf

--- components/HomeComponent/Slides.js
+++ components/HomeComponent/Slides.js
@@ -21,42 +21,28 @@ const Slides = () => {
         <Carousel.Item className='carousel-slider slider-1'>
           <div className='slider-text'>
             <h1>
-              Bureau d'étude <br /> pludisciplinaire
+            Étude VRD
             </h1>
             <h3>
-              AGEXIS est une société d'ingénierie <br /> et de gestion des
-              projets <br />
-              de la construction
+            Réaliser des études de VRD  <br /> pour Ouvrage d’art <br />
+             et le déploiement d’infrastructure.
             </h3>
             <button className='second-btn'>Devis en ligne</button>
           </div>
         </Carousel.Item>
         <Carousel.Item className='carousel-slider slider-2'>
           <div className='slider-text'>
-            <h1>
-              Bureau d'étude <br /> pludisciplinaire
+          <h1>
+           Étude électricité
             </h1>
             <h3>
-              AGEXIS est une société d'ingénierie <br /> et de gestion des
-              projets <br />
-              de la construction
+            Réaliser des études en électricité <br />
+             pour des ouvrages neufs ou réhabilité.
             </h3>
             <button className='second-btn'>Devis en ligne</button>
           </div>
         </Carousel.Item>
-        <Carousel.Item className='carousel-slider slider-0'>
-          <div className='slider-text'>
-            <h1>
-              Bureau d'étude <br /> pludisciplinaire
-            </h1>
-            <h3>
-              AGEXIS est une société d'ingénierie <br /> et de gestion des
-              projets <br />
-              de la construction
-            </h3>
-            <button className='second-btn'>Devis en ligne</button>
-          </div>
-        </Carousel.Item>
+     
         {/* <Carousel.Item className='carousel-slider slider-3'>
           <div className='slider-text'>
             <h1>
